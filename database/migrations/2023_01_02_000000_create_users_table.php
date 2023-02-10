9dc6a55b071f5fbf9fff67916fb505eb717a21e7
Jehu Casimiro <jehu.casimiro16@gmail.com>
2023-02-10 22:21:26 +0800

    fix users migration script

--- database/migrations/2023_01_02_000000_create_users_table.php
+++ database/migrations/2023_01_02_000000_create_users_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignId('role_id')->constrained()->default(1)->onDelete('cascade')->onUpdate('cascade');
-            $table->foreignId('program_id')->constrained()->nullable()->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('role_id')->default(1)->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('program_id')->nullable()->constrained()->onDelete('cascade')->onUpdate('cascade');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
