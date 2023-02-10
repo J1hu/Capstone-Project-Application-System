8da805becaf792d2104fb87121c1d4b435569ecc
Jehu Casimiro <jehu.casimiro16@gmail.com>
2023-02-10 22:03:34 +0800

    added default value for roles

--- database/migrations/2023_01_02_000000_create_users_table.php
+++ database/migrations/2023_01_02_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignId('role_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('role_id')->constrained()->default(1)->onDelete('cascade')->onUpdate('cascade');
             $table->foreignId('program_id')->constrained()->nullable()->onDelete('cascade')->onUpdate('cascade');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
