222ad948d1d75a58afbc65b8114f0dd92c9e0dd9
Jehu Casimiro <jehu.casimiro16@gmail.com>
2023-02-12 16:27:24 +0800

    removed typo

--- app/Models/ApplicantMother.php
+++ app/Models/ApplicantMother.php
@@ -5,8 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-use function PHPSTORM_META\map;
-
 class ApplicantMother extends Model
 {
     use HasFactory;
