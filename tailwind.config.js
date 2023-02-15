d59440d153b74113de5772efc1e386125e444279
Andrea Maurice <andreamauricedg@gmail.com>
2023-02-15 20:25:43 +0800

    tailwind config and updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,11 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
+        './resources/views/**/*.blade.php',
+        './resources/views/*.blade.php',
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        './app/**/*.blade.php',
     ],
 
     theme: {
