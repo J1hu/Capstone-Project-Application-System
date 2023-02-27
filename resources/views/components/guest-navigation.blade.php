2added10af0bc65e00111fb2a078189e6c7d2c12
Andrea Maurice <andreamauricedg@gmail.com>
2023-02-27 21:42:33 +0800

    changes on responsiveness, added favicon

--- resources/views/components/guest-navigation.blade.php
+++ resources/views/components/guest-navigation.blade.php
@@ -3,7 +3,7 @@
         <div>
                 <img src="{{ asset('assets/lvlogo.png') }}" alt="La Verdad Christian College Inc. Logo" width="70">
         </div>
-        <div class="hidden md:grid content-center">
+        <div class="hidden content-center md:grid">
             <p class="font-bold sm:text-lg">LA VERDAD CHRISTIAN COLLEGE</p>
             <p class="italic leading-4">McArthur Highway, Brgy. Sampaloc, Apalit, Pampanga</p>
         </div>
@@ -11,7 +11,7 @@
     <div class="text-center flex justify-end items-center">
         <a href="{{ route('login') }}">
             <div
-            {{ $attributes->merge(['class' => 'w-24 sm:w-36 text-center p-2 lg:font-bold rounded-sm text-sm '. $login]) }}>
+            {{ $attributes->merge(['class' => 'w-24 text-center p-2 sm:w-36 lg:font-bold rounded-sm text-sm '. $login]) }}>
                 LOGIN
             </div>
         </a>
