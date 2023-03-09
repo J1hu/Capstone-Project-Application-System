789f0dd9584fc9e7508c7f4797813e147b798b56
Andrea Maurice <andreamauricedg@gmail.com>
2023-03-09 21:35:32 +0800

    added privacy contents

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -3,6 +3,40 @@
 <div class="h-fit my-5 mx-10 sm:mx-14 sm:my-7 md:mx-20 md:m7-9 lg:mx-32 lg:my-10">
     <div class="title text-center font-bold text-2xl sm:text-3xl">PRIVACY POLICY</div>
     <div class="mt-5 text-justify sm:mt-7 md:my-10">
+        <p>We are the official website for La Verdad Christian College’s Scholarship Application System. We have developed this Privacy Policy to notify you about our privacy practices. We value your rights to privacy that any personal information collected on or through this site will be treated in accordance with Republic Act No. 10173, or the Data Privacy Act of 2012, and its Implementing Rules and Regulations.</p>
+        <br>
+        <p>By using this system, you consent to the collection, and processing of personal information to improve the content of our web pages and the overall quality of our services.</p>
+        <br>
+        <p>Other domains (e.g., websites, microsites, apps, etc.) managed or maintained by the LVCC, including those by affiliated units, may be covered by a separate and different policy.</p>
+        <br>
+        <p class="font-bold">Information Gathering and Usage</p>
+        <p>As part of the security protocol that uses firewall, this site automatically records technical information about your visit.</p><br>
+        <p>LVCC uses the technical information for the following general purposes: products and services provision, geocoding and location mapping, identification and authentication, services improvement, and research.</p><br>
+        <p class="font-bold">Cookies</p>
+        <p>A cookie is a small amount of data, which often includes an anonymous unique identifier that is sent to your browser from a web site’s computers and stored on your device memory.</p>
+        <p>Cookies are required to use the LVCC service. We use cookies to record current session information. You are required to log-in to your LVCC site after a certain period of time has elapsed to protect you against others accidentally accessing your account contents.</p><br>
+        <p class="font-bold">Storage and Retention</p>
+        <p>LVCC uses third party vendors and hosting partners to provide the necessary hardware, software, networking, storage, and related technology required to run LVCC.</p><br>
+        <p>nformation collected are retained until its declared purpose has been achieved.</p><br>
+        <p class="font-bold">Sharing and Disclosure</p>
+        <p>Information is not shared with or sold to other organizations for commercial purposes, except to provide products or services you have requested, when we have your permission, or under the following circumstances:</p><br>
+        <ol class="list-decimal list-inside">
+            <li>
+                It is necessary to share information in order to investigate, prevent, or take action regarding illegal activities, suspected fraud, situations involving potential threats to the physical safety of any person, violations of our Privacy Policy, or as otherwise required by law.</li>
+            <li>We transfer information about you if LVCC is acquired by or merged with another company. In this event, LVCC will notify you before information about you is transferred and becomes subject to a different policy</li>
+        </ol><br>
+        <p>LVCC however, may disclose personal information under special circumstances, such as to comply with subpoenas or when your actions violate the Privacy Policy.</p><br>
+        <p class="font-bold">Security</p>
+        <p>We have adopted the technical and organisational measures necessary to ensure the security of the personal data we collect, use and maintain, and prevent their alteration, loss, unauthorised processing or access, having regard to the state of the art, the nature of the data stored and the risks to which they are exposed by virtue of human action or physical or natural environment. However, as effective as our security measures are, no security system is impenetrable. We cannot guarantee the security of our database, nor can we guarantee that personal information you supply will not be intercepted while being transmitted to us over the internet. We ask that you do your part by maintaining in strict confidence any computer passwords you use to access the Internet or through mobile applications. We encourage responsible reporting of any vulnerabilities that may be found in our webpage or any application available through our webpage.</p><br>
+        <p class="font-bold">Your rights</p>
+        <p>You have the right to access personal information held about you in accordance with the local applicable laws, any such access request may be subject to a fee to meet our costs in providing you with details of the personal information that we hold about you. Subject to the Data Privacy Act, you shall have the right to object to any processing that is likely to cause you damage or distress and you also have the right to rectify, block, erase or destroy inaccurate data.</p><br>
+        <p>For any questions and/or concerns about this Privacy Policy, please contact:<br>
+            The Data Protection Officer<br>
+            LA VERDAD CHRISTIAN COLLEGE, INC.<br>
+            MacArthur Highway, Sampaloc, Apalit, Pampanga<br>
+            Email: dpo@laverdad.edu.ph</p><br>
+        <p class="font-bold">Changes</p>
+        <p>LVCC reserves the right to update this Policy as we see fit. We will notify you about significant changes by sending a notice to the primary email address specified in your LVCC account or by placing a prominent notice on our site. If you are a regular visitor to this system, we recommend that you check this Privacy Policy on a regular basis.</p>
     </div>
 </div>
 @include('foot')
