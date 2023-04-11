1a9cf7cc4a467e0dbcf6d13810bc67fca86a3fa6
J1hu <jehu.casimiro16@gmail.com>
2023-04-11 21:29:42 +0800

    init applicant form

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -4,11 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class ApplicantController extends Controller
 {
     public function profile($id)
     {
         //
+    }
+
+    public function viewForm()
+    {
+        return view('applicants.form');
+    }
+
+    public function view(Request $request): View
+    {
+        return view('applicants.profile', [
+            'user' => $request->user(),
+        ]);
     }
 }
