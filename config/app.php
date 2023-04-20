606a529f818395fd4c97d78ed90448b4532f7036
Andrea Maurice <andreamauricedg@gmail.com>
2023-04-20 21:42:20 +0800

    changed local server urls to ngrok url

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://662f-124-104-32-25.ngrok-free.app/'),
 
     'asset_url' => env('ASSET_URL'),
 
