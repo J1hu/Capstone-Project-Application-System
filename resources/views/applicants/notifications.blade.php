8c3ac0abfbb3dc7c3ef8f9af5ba31e1d4f06bad9
Jehu Casimiro <jehu.casimiro16@gmail.com>
2023-05-13 01:58:06 +0800

    added rich text editor to notifications

--- resources/views/applicants/notifications.blade.php
+++ resources/views/applicants/notifications.blade.php
@@ -1,20 +1,18 @@
 <x-app-layout>
-    <div class="container">
-        <h2>Notifications</h2>
-        @forelse ($notifications as $notification)
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Title: {{ $notification->data['title'] }}</h5>
-                <p class="card-text">Content: {{ $notification->data['content'] }}</p>
-                <form action="{{ route('notifications.mark-as-read', $notification->id) }}" method="POST">
-                    @csrf
-                    @method('PUT')
-                    <button type="submit" class="btn btn-primary">Mark as Read</button>
-                </form>
-            </div>
+    <h2>Notifications</h2>
+    @forelse ($notifications as $notification)
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Title: {{ $notification->data['title'] }}</h5>
+            <p>Content:</p> {!! $notification->data['content'] !!}
+            <form action="{{ route('notifications.mark-as-read', $notification->id) }}" method="POST">
+                @csrf
+                @method('PUT')
+                <button type="submit" class="btn btn-primary">Mark as Read</button>
+            </form>
         </div>
-        @empty
-        <p>No notifications found.</p>
-        @endforelse
     </div>
+    @empty
+    <p>No notifications found.</p>
+    @endforelse
 </x-app-layout>
