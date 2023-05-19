df77979ad65a3e39eedea52de2e927a41cd69b21
Jehu Casimiro <jehu.casimiro16@gmail.com>
2023-05-19 21:33:43 +0800

    done searchables

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,15 +13,48 @@
     <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
     {{-- Icons --}}
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <x-rich-text-trix-styles />
+
+    <!-- Datatables -->
+    <link rel="stylesheet" href="//cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
 </head>
 
 <body class="antialiased bg-gray-100">
+    <style>
+        /*custom styling */
+        #myTable_wrapper {
+            padding: 10px;
+        }
+
+        #myTable_filter input[type="search"] {
+            border: 1px solid #ccc;
+            border-radius: 10px;
+            padding: 6px;
+        }
+
+        #myTable_length select {
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            padding-left: 5px;
+            padding-right: 30px;
+        }
+
+        .odd {
+            background-color: #fff;
+        }
+
+        .even {
+            background-color: #f8f8f8;
+        }
+
+        .resident-row:hover {
+            background-color: #e5f7f7;
+        }
+    </style>
     <div class="relative dark:bg-gray-900">
         @include('layouts.navigation')
         @include('layouts.sidenavigation')
@@ -30,17 +63,16 @@
         <main class="right-0 py-6 px-4 sm:px-6 lg:px-8 w-4/5 absolute top-20">
             <!-- Page Heading -->
             @if (isset($header))
-                <header>
-                    <div class="max-w-7xl mx-auto grid grid-cols-2">
-                        {{ $header }}
-                    </div>
-                </header>
+            <header>
+                <div class="max-w-7xl mx-auto grid grid-cols-2">
+                    {{ $header }}
+                </div>
+            </header>
             @endif
             {{ $slot }}
         </main>
     </div>
-    <script>
-        @yield('scripts')
-    </script>
+    @yield('scripts')
 </body>
+
 </html>
--- resources/views/staffs/list.blade.php
+++ resources/views/staffs/list.blade.php
@@ -3,7 +3,7 @@
     <x-slot name="header">
         <h2 class="font-bold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Registrar Staffs') }}
-        </h1>
+            </h1>
     </x-slot>
     {{-- Table & Spacings --}}
     <div class="text-right">
@@ -16,7 +16,7 @@
                     <h1 class="font-bold p-6">List of Registrar Staffs</h1>
                 </div>
                 @auth
-                <table class="w-full py-3 table-auto">
+                <table class="w-full py-3 table-auto" id="myTable">
                     <thead class="bg-slate-100 text-left">
                         <tr>
                             <th class="py-2 px-6">Name</th>
@@ -42,8 +42,20 @@
         </div>
     </div>
 
-
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+    <script src="//cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
     <script>
+        $(document).ready(function() {
+            let table = $('#myTable').DataTable({
+                "lengthMenu": [10, 25, 50, 75, 100],
+                "pageLength": 25,
+                "columnDefs": [{
+                    "orderable": false,
+                    "targets": -1
+                }]
+            });
+        });
+
         document.getElementById('create-staff-btn').addEventListener('click', function() {
             window.location.href = "{{ route('staffs.create') }}";
         });
