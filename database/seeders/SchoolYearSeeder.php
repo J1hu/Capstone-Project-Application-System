d829b45a0a345bdef0b7c35e7181eca3066dd7da
Jehu Casimiro <jehu.casimiro16@gmail.com>
2023-05-22 17:02:45 +0800

    school year management logic update

---
+++ database/seeders/SchoolYearSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\SchoolYear;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SchoolYearSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        SchoolYear::factory()->times(1)->create();
+    }
+}
