c3ef27a32c750eea1ac78bfd6b770ba16b87745c
Andrea Maurice <andreamauricedg@gmail.com>
2023-05-24 01:54:13 +0800

    initial changes

--- app/Http/Controllers/VisualizationController.php
+++ app/Http/Controllers/VisualizationController.php
@@ -3,11 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Applicant;
+use DB;
 
 class VisualizationController extends Controller
 {
     public function index()
     {
-        return view('admin.visualization');
+        $female = Applicant::where('sex', 'Female')->count();
+        $male = Applicant::where('sex', 'Male')->count();
+
+        $bsis = Applicant::where('program_id', 1)->count();
+        $act = Applicant::where('program_id', 2)->count();
+        $bsais = Applicant::where('program_id', 3)->count();
+        $bsa = Applicant::where('program_id', 4)->count();
+        $bab = Applicant::where('program_id', 5)->count();
+        $bssw = Applicant::where('program_id', 6)->count();
+        $all = Applicant::count();
+
+        return view('admin.visualization', compact(
+            'female',
+            'male',
+            'bsis',
+            'act',
+            'bsais',
+            'bsa',
+            'bab',
+            'bssw',
+            'all',
+        ));
     }
 }
