e18d9750ca6a9c9a4df91f1182890bf305d9368e
Justine Macabuhay <justinemacabuhay@student.laverdad.edu.ph>
2023-05-24 22:46:00 +0800

    faqs

--- resources/views/faqs.blade.php
+++ resources/views/faqs.blade.php
@@ -3,6 +3,97 @@
 <div class="h-fit my-5 mx-10 sm:mx-14 sm:my-7 md:mx-20 md:m7-9 lg:mx-32 lg:my-10">
     <div class="title text-center font-bold text-2xl sm:text-3xl">FREQUENTLY ASKED QUESTIONS</div>
     <div class="mt-5 text-justify sm:mt-7 md:my-10">
+        <!-- component -->
+<!-- 
+    FAQ - Frequently Asked Questions TailwindCSS Component
+    with <details> and <summary> tag with custom [open] animation.
+    Created by Surjith S M (@surjithctly)
+    See more components: https://web3components.dev 
+-->
+
+<div class="max-w-screen-xl mx-auto px-5 bg-white min-h-sceen">
+	<div class="grid divide-y divide-neutral-200 max-w-xxl mx-auto mt-8">
+		<div class="py-7">
+			<details class="group">
+				<summary class="flex justify-between items-center font-medium cursor-pointer list-none">
+					<span> How to apply for scholarship?</span>
+					<span class="transition group-open:rotate-180">
+                <svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path>
+                </svg>
+              </span>
+				</summary>
+				<p class="text-neutral-600 mt-3 group-open:animate-fadeIn">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel feugiat leo. 
+                 Vivamus lobortis pretium maximus. Ut dignissim tellus eget lacus rutrum, in sollicitudin enim molestie.
+				</p>
+			</details>
+		</div>
+		<div class="py-7">
+			<details class="group">
+				<summary class="flex justify-between items-center font-medium cursor-pointer list-none">
+					<span> What are the programs available?</span>
+					<span class="transition group-open:rotate-180">
+                <svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path>
+                </svg>
+              </span>
+				</summary>
+				<p class="text-neutral-600 mt-3 group-open:animate-fadeIn">
+                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel feugiat leo. 
+                Vivamus lobortis pretium maximus. Ut dignissim tellus eget lacus rutrum, in sollicitudin enim molestie.
+				</p>
+			</details>
+		</div>
+		<div class="py-7">
+			<details class="group">
+				<summary class="flex justify-between items-center font-medium cursor-pointer list-none">
+					<span> Can I change my information after submission?</span>
+					<span class="transition group-open:rotate-180">
+                <svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path>
+                </svg>
+              </span>
+				</summary>
+				<p class="text-neutral-600 mt-3 group-open:animate-fadeIn">
+                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel feugiat leo. 
+                Vivamus lobortis pretium maximus. Ut dignissim tellus eget lacus rutrum, in sollicitudin enim molestie.
+				</p>
+			</details>
+		</div>
+		<div class="py-7">
+			<details class="group">
+				<summary class="flex justify-between items-center font-medium cursor-pointer list-none">
+					<span> How to know which scholarship type is the best for me to apply for? (applicable if allowing choosing of scholarship type)</span>
+					<span class="transition group-open:rotate-180">
+                <svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path>
+                </svg>
+              </span>
+				</summary>
+				<p class="text-neutral-600 mt-3 group-open:animate-fadeIn">
+				 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel feugiat leo. 
+                Vivamus lobortis pretium maximus. Ut dignissim tellus eget lacus rutrum, in sollicitudin enim molestie.
+				</p>
+			</details>
+		</div>
+		<div class="py-7">
+			<details class="group">
+				<summary class="flex justify-between items-center font-medium cursor-pointer list-none">
+					<span> How do I know if my scholarship application is accepted or rejected/if I passed the evaluation or not?</span>
+					<span class="transition group-open:rotate-180">
+                <svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path>
+                </svg>
+              </span>
+				</summary>
+				<p class="text-neutral-600 mt-3 group-open:animate-fadeIn">
+                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel feugiat leo. 
+                Vivamus lobortis pretium maximus. Ut dignissim tellus eget lacus rutrum, in sollicitudin enim molestie.
+				</p>
+			</details>
+		</div>
+	</div>
+</div>
+
+
+    
     </div>
 </div>
+
 </x-body>
