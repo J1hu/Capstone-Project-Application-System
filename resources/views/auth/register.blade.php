312cf40a09dae7bf520416a1d82f43df6a2b5157
Andrea Maurice <andreamauricedg@gmail.com>
2023-05-29 22:07:17 +0800

    show and hide pw fix

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -20,24 +20,30 @@
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+            <div class="flex items-center mt-1 w-full border border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 rounded-md shadow-sm">
+                <input id="password" class="flex-1 w-full p-2 border-0 rounded-l-md focus:outline-none" type="password" name="password" required autocomplete="new-password">
+                <span class="material-symbols-outlined p-2 bg-blue-500 rounded-r-md border-blue-500 text-white" id="togglePassword">
+                  visibility
+                </span>
+              </div>
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+<div class="mt-4">
+    <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
+    <div class="flex items-center mt-1 w-full border border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 rounded-md shadow-sm">
+        <input id="password_confirmation" class="flex-1 w-full p-2 border-0 rounded-l-md focus:outline-none" type="password" name="password_confirmation" required autocomplete="new-password">
+        <span class="material-symbols-outlined p-2 bg-blue-500 rounded-r-md border-blue-500 text-white" id="toggleConfirmPassword">
+          visibility
+        </span>
+    </div>
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
+    <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+</div>
+
 
         <div class="flex flex-col-reverse items-center justify-end mt-5">
             <a class="text-sm text-gray-600 dark:text-blue-400 hover:text-blue-900 dark:hover:text-gray-100 rounded-md" href="{{ route('login') }}">
@@ -49,4 +55,37 @@
             </x-primary-button>
         </div>
     </form>
+    <script>
+        window.addEventListener("DOMContentLoaded", function() {
+            const togglePassword = document.querySelector("#togglePassword");
+            const toggleConfirmPassword = document.querySelector("#toggleConfirmPassword");
+            const password = document.querySelector("#password");
+            const confirmPassword = document.querySelector("#password_confirmation");
+    
+            togglePassword.addEventListener("click", function() {
+                // Toggle the password visibility
+                const type = password.getAttribute("type") === "password" ? "text" : "password";
+                password.setAttribute("type", type);
+    
+                if (togglePassword.innerHTML === "visibility") {
+                    togglePassword.innerHTML = "visibility_off";
+                } else {
+                    togglePassword.innerHTML = "visibility";
+                }
+            });
+    
+            toggleConfirmPassword.addEventListener("click", function() {
+                // Toggle the confirm password visibility
+                const type = confirmPassword.getAttribute("type") === "password" ? "text" : "password";
+                confirmPassword.setAttribute("type", type);
+    
+                if (toggleConfirmPassword.innerHTML === "visibility") {
+                    toggleConfirmPassword.innerHTML = "visibility_off";
+                } else {
+                    toggleConfirmPassword.innerHTML = "visibility";
+                }
+            });
+        });
+    </script>
+    
 </x-guest-layout>
