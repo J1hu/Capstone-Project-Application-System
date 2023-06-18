49157b0d76bd1ee07a59f168cfbcff4ed0889a37
Jehu Casimiro <jehu.casimiro16@gmail.com>
2023-06-19 02:35:08 +0800

    added select2 on selecting applicants for notification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
     <!-- External Script -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script type="text/javascript" src="/script.js"></script>    
+    <script type="text/javascript" src="/script.js"></script>
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
--- resources/views/notifications/send-applicant.blade.php
+++ resources/views/notifications/send-applicant.blade.php
@@ -1,4 +1,9 @@
 <x-app-layout>
+
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+        <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    </head>
     <x-slot name="header">
         <h2 class="font-bold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Create a Notification') }}
@@ -15,13 +20,10 @@
                 <div class="form-group m-1 grid grid-cols-2">
                     <div class="grid">
                         <label for="applicant">Select a Specific Applicant:</label>
-                        <select name="applicant" id="applicant"
-                            class="inline-flex items-center m-1 p-3 text-sm leading-5 text-black border-2 rounded-md border-slate-400 bg-white focus:ring-blue-500 focus:border-blue-500 w-full">
+                        <select name="applicant" id="applicant" class="js-example-basic-single inline-flex items-center m-1 p-3 text-sm leading-5 text-black border-2 rounded-md border-slate-400 bg-white focus:ring-blue-500 focus:border-blue-500 w-full">
                             <option selected disabled>Select an applicant to message</option>
                             @foreach ($applicants as $applicant)
-                                <option
-                                    class="inline-flex items-center m-1 p-3 text-sm leading-5 text-black border-2 rounded-md border-slate-400 bg-white focus:ring-blue-500 focus:border-blue-500"
-                                    value="{{ $applicant->id }}">{{ $applicant->getFullNameAttribute() }}</option>
+                            <option class="inline-flex items-center m-1 p-3 text-sm leading-5 text-black border-2 rounded-md border-slate-400 bg-white focus:ring-blue-500 focus:border-blue-500" value="{{ $applicant->id }}">{{ $applicant->getFullNameAttribute() }}</option>
                             @endforeach
                         </select>
                     </div>
@@ -30,9 +32,7 @@
 
             <div class="form-group mt-3 grid">
                 <label for="title">Title:</label>
-                <input
-                    class="inline-flex items-center m-1 p-3 text-sm leading-5 text-black border-2 rounded-md border-slate-400 bg-white focus:ring-blue-500 focus:border-blue-500 w-full"
-                    type="text" name="title" id="title" required>
+                <input class="inline-flex items-center m-1 p-3 text-sm leading-5 text-black border-2 rounded-md border-slate-400 bg-white focus:ring-blue-500 focus:border-blue-500 w-full" type="text" name="title" id="title" required>
             </div>
             <div class="form-group mt-5">
                 <label for="content">Content:</label>
@@ -41,4 +41,9 @@
             </div>
         </div>
     </form>
+    <script>
+        $(document).ready(function() {
+            $('.js-example-basic-single').select2();
+        });
+    </script>
 </x-app-layout>
